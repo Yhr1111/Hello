66be7b8eb75f0db961061cc699f3e9bd1eff37ad
Yhr1111 <516405255@qq.com>
2023-01-18 17:00:51 +0800

    Merge branch 'main' of https://github.com/Yhr1111/Hello
    # Conflicts:
    #	.gitignore

--- src/main/java/com/yang/hello1/controller/helloController.java
+++ src/main/java/com/yang/hello1/controller/helloController.java
@@ -1,11 +1,12 @@
 package com.yang.hello1.controller;
 
+
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("hello")
+@RequestMapping("hello1")
 public class helloController {
 
 
